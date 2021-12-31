1eb32c4e90a0f19348a266b893f9e993f6dbe215
Alex Danilo <flasher2085@gmail.com>
2021-12-31 09:47:14 +0100

    Heroku build task

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
     API: {
         HOST: process.env.API_HOST || 'http://localhost:3004',
-        API_PREFIX: process.env.API_HOST || 'api',
+        API_PREFIX: process.env.API_PREFIX || 'api',
         VERSION: process.env.API_VERSION || 'v1',
         ENDPOINTS: {
             SEARCH_PERSONS: 'persons/search',
