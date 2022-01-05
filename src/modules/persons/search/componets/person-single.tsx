a7a1a1a883becf6f94571f4ba0fbdcaae914a8a1
Alex Danilo <flasher2085@gmail.com>
2022-01-05 21:28:26 +0100

    search result highlight

--- src/modules/persons/search/componets/person-single.tsx
+++ src/modules/persons/search/componets/person-single.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Highlighter from 'react-highlight-words';
 import { PersonSingleRating } from '@modules/persons/search/componets/person-single-rating';
 import { UiButton, UiButtonSize } from '@core/components/ui/ui-button';
 import { UiGlobalColorScheme } from '@core/constants/ui-constants';
 import { ISearchResponsePerson } from '@core/services/api/endpoints/persons-api-class';
-import { StringService, WrapTag } from '@root/core/services/string-service';
 
 import styles from '../styles/person-single.module.scss';
 
@@ -18,14 +18,13 @@ export const PersonSingle: React.FC<Props> = React.memo((
         search,
         person
     }) => {
-    const wrappedSearch = React.useMemo(() => {
-        return StringService.wrapSubstring(person.name, search, WrapTag.span);
-    }, [search]);
     return (
         <div className={styles.container}>
-            <div
-                className={styles.name}
-                dangerouslySetInnerHTML={{__html: wrappedSearch}}
+            <Highlighter
+                highlightClassName={styles.highlight}
+                searchWords={[search]}
+                autoEscape={true}
+                textToHighlight={person.name}
             />
             <div className={styles.info}>
                 <PersonSingleRating rating={person.followers}/>
@@ -38,5 +37,5 @@ export const PersonSingle: React.FC<Props> = React.memo((
                 </Link>
             </div>
         </div>
-    )
-})
+    );
+});
